72807f620e3ac9162abf94ebdaa8138605c0feba
Jeff Dutra <jeff.dutra@gmail.com>
2014-04-26 10:02:01 -0400

    Moved ValidateEntity into its own class

--- EntityAndValueObjectBase/EntityAndValueObjectBase.Infrastructure/DomainValidation/IValidateEntity.cs
+++ EntityAndValueObjectBase/EntityAndValueObjectBase.Infrastructure/DomainValidation/IValidateEntity.cs
@@ -12,37 +12,4 @@ namespace EntityAndValueObjectBase.Infrastructure.DomainValidation
         void ThrowExceptionIfEntityIsInvalid(EntityBase<string> entity);
         void ThrowExceptionIfEntityIsInvalid(EntityBase<Guid> entity);
     }
-
-    public class ValidateEntity : IValidateEntity
-    {
-        public bool IsValid(EntityBase<int> entity)
-        {
-            return entity.IsValid();
-        }
-
-        public bool IsValid(EntityBase<string> entity)
-        {
-            return entity.IsValid();
-        }
-
-        public bool IsValid(EntityBase<Guid> entity)
-        {
-            return entity.IsValid();
-        }
-
-        public void ThrowExceptionIfEntityIsInvalid(EntityBase<int> entity)
-        {
-            entity.ThrowExceptionIfEntityIsInvalid();
-        }
-
-        public void ThrowExceptionIfEntityIsInvalid(EntityBase<string> entity)
-        {
-            entity.ThrowExceptionIfEntityIsInvalid();
-        }
-
-        public void ThrowExceptionIfEntityIsInvalid(EntityBase<Guid> entity)
-        {
-            entity.ThrowExceptionIfEntityIsInvalid();
-        }
-    }
 }
