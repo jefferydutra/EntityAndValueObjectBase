b3364411115b652621a3224def496d102fdddc90
Jeff Dutra <jeff.dutra@gmail.com>
2014-04-26 09:38:03 -0400

    Added IValidateEntity w/ concrete implementation
    This will abstract out the extension methods and allow you to program to
    an interface.  The concrete implementation just calls the
    ValidateEntityExtensions methods

--- EntityAndValueObjectBase/EntityAndValueObjectBase.Infrastructure/DomainValidation/IValidateEntity.cs
+++ EntityAndValueObjectBase/EntityAndValueObjectBase.Infrastructure/DomainValidation/IValidateEntity.cs
@@ -1,8 +1,18 @@
-using System;
+﻿using System;
 using EntityAndValueObjectBase.Infrastructure.Domain;
 
 namespace EntityAndValueObjectBase.Infrastructure.DomainValidation
 {
+    public interface IValidateEntity
+    {
+        bool IsValid(EntityBase<int> entity);
+        bool IsValid(EntityBase<string> entity);
+        bool IsValid(EntityBase<Guid> entity);
+        void ThrowExceptionIfEntityIsInvalid(EntityBase<int> entity);
+        void ThrowExceptionIfEntityIsInvalid(EntityBase<string> entity);
+        void ThrowExceptionIfEntityIsInvalid(EntityBase<Guid> entity);
+    }
+
     public class ValidateEntity : IValidateEntity
     {
         public bool IsValid(EntityBase<int> entity)
